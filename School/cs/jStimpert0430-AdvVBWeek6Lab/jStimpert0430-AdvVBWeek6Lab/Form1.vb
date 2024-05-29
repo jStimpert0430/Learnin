2eb0a5873b64ba004e21dc1303b6d099046a7705
Tofu Shop <jStimpert0430@gmail.com>
2024-05-29 12:33:18 -0400

    Added csharp schoolwork

--- School/cs/jStimpert0430-AdvVBWeek6Lab/jStimpert0430-AdvVBWeek6Lab/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-    Private dl As labDBClass1
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        TextBox1.Text = dl.getXML()
-    End Sub
-End Class
